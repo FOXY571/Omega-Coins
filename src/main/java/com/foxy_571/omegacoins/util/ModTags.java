3138359ff5657adef29e55ade935d2f3210b1ec4
FOXY_571 <thomastowson2007@gmail.com>
2025-04-19 16:22:50 -0500

    Added coins tag

--- src/main/java/com/foxy_571/omegacoins/util/ModTags.java
+++ src/main/java/com/foxy_571/omegacoins/util/ModTags.java
@@ -3,7 +3,9 @@ package com.foxy_571.omegacoins.util;
 import com.foxy_571.omegacoins.OmegaCoins;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 public class ModTags {
@@ -12,6 +14,14 @@ public class ModTags {
 
         private static TagKey<Block> createTag(String name) {
             return BlockTags.create(ResourceLocation.fromNamespaceAndPath(OmegaCoins.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+        public static final TagKey<Item> COINS = createTag("coins");
+
+        private static TagKey<Item> createTag(String name) {
+            return ItemTags.create(ResourceLocation.fromNamespaceAndPath(OmegaCoins.MOD_ID, name));
         }
     }
 }
