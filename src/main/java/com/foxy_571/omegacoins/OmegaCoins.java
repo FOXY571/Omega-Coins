13420f349b5534636e9228498d089c5986b7f6bc
Thomas Towson <thomastowson2007@gmail.com>
2025-04-19 14:49:11 -0500

    Fixed gitbarf

--- src/main/java/com/foxy_571/omegacoins/OmegaCoins.java
+++ src/main/java/com/foxy_571/omegacoins/OmegaCoins.java
@@ -1,8 +1,16 @@
 package com.foxy_571.omegacoins;
 
 import com.foxy_571.omegacoins.block.ModBlocks;
+import com.foxy_571.omegacoins.block.entity.ModBlockEntities;
+import com.foxy_571.omegacoins.block.entity.renderer.CoinPressBlockEntityRenderer;
 import com.foxy_571.omegacoins.item.ModCreativeModeTabs;
 import com.foxy_571.omegacoins.item.ModItems;
+import com.foxy_571.omegacoins.recipes.ModRecipes;
+import com.foxy_571.omegacoins.util.ModTags;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -20,5 +28,15 @@ public class OmegaCoins {
         ModCreativeModeTabs.register(modEventBus);
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
+        ModBlockEntities.register(modEventBus);
+        ModRecipes.register(modEventBus);
+    }
+
+    @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void registerBER(EntityRenderersEvent.RegisterRenderers event) {
+            event.registerBlockEntityRenderer(ModBlockEntities.COIN_PRESS.get(), CoinPressBlockEntityRenderer::new);
+        }
     }
 }
